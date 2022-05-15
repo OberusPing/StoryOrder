81f695422abd3ff8d186e4df434e91bd5d3a6ea1
Achyuth <achyuthc@gmail.com>
2022-05-15 12:03:14 -0400

    Initial implementation.

---
+++ main.py
@@ -0,0 +1,34 @@
+import nltk
+from nltk.tokenize import sent_tokenize
+import argparse
+
+nltk.download('punkt')
+
+def checker(file_path):
+    """
+    Checks if file_path ends with '.txt'
+    :param file_path: str
+    :return: str
+    """
+    if not file_path.endswith('.txt'):
+        raise argparse.ArgumentTypeError('File must have extension .txt')
+    return file_path
+
+# Use argparse to help construct a clean CLI
+parser = argparse.ArgumentParser(description='Sort all sentences of an input text file in alphabetical order.')
+parser.add_argument('File',
+                    metavar='file_name',
+                    type=checker,
+                    help='the text file to order')
+args = parser.parse_args()
+
+# Iterates through lines in input file, tokenizing each into sentences, then outputs each sentence as a line in a new
+# file, in alphabetical order
+with open(args.File + "_ordered.txt", 'w') as outfile, open(args.File, 'r') as infile:
+    all_sentences = []
+    for line in infile:
+        line = line.replace('"', '“')
+        all_sentences.extend(sent_tokenize(line))
+    for sentence in sorted(all_sentences):
+        outfile.write(sentence)
+        outfile.write('\n')
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Story Order
+
+## Dependencies
+
+- pip
+
+## Getting Started
+
+```bash
+# Install Dependencies
+pip install
+
+# Run Application on sample Short Story
+python main.py ShortStory.txt
+```
